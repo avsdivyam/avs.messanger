e7c8169bceff2e67f37c4525e6e2333a93f72bdd
divyam <tyagidav97@gmail.com>
2025-06-17 10:10:34 +0000

    refactoring files

--- src/components/spinner.tsx
+++
@@ -1,7 +0,0 @@
-export function Spinner() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="animate-spin h-6 w-6 border-4 border-blue-600 border-t-transparent rounded-full" />
-    </div>
-  );
-}
